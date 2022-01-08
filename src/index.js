1388735879090a3a27456aae61f0c98db73cc50a
anmaiden <aannna123@gmail.com>
2022-01-08 18:22:41 +0300

    feat: add a task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+ if (matrix === undefined) {
+   return [];
+ }
+ let res = [];
+ for(let i = 0; i < matrix.length; i++){ // пробегаемся по двумерному массиву
+   if (i % 2 === 0){ // если кол-во элементов четное
+     for(let j = 0; j < matrix[i].length; j++){
+       res.push(matrix[i][j]); // пушим в переменную результат - новый массив
+     }
+   }else{ // если матрица нечетная (кол-во элементов i)
+     for(let a = matrix[i].length - 1; a >= 0; a--) { // пробегаемся по массиву, исключая несовпадения по длине
+        res.push(matrix[i][a]); 
+     }
+   }
+ }
+ return res;
+};
